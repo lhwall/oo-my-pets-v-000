ed4994e15a8e8ba4fd302709359fc01bf60c6541
Lucy Wall <lhwall@gmail.com>
2018-04-05 15:23:27 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
   # code goes here
+  @@all = []
+  
+  def initialize(name = nil)
+    @name=name 
+  end
+   
+  def create(name = nil) 
+    new_owner = self.new 
+    new_owner.save 
+  end 
+  
+  def save 
+    @@all << self
+  end 
+  
+  def self.clear_all 
+    @@all.clear 
+  end
+    
+
 end
