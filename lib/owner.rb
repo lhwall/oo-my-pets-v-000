746a217d24b85812f7b8cd432522e8a096b0ef24
Lucy Wall <lhwall@gmail.com>
2018-04-05 15:41:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
 class Owner
   # code goes here
-  attr_accessor pets
-  attr_reader species
+  attr_accessor :pets, :name
+  attr_reader :species
   @@all = []
   
   def initialize(name = nil)
@@ -38,6 +38,8 @@ class Owner
     @pets:cats <<
   end
   
+  def say_name
+    
     
 
 end
