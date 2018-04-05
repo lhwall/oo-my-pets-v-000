bf664d5edc72862f6fcc4837fa7c0f60b9b5bf0f
Lucy Wall <lhwall@gmail.com>
2018-04-05 15:50:24 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,13 +34,18 @@ class Owner
   end
   
   def buy_cat(name)
-    new_cat = Cat.new 
-    @pets[:cats] << self
+    new_cat = Cat.new(name)
+    @pets[:cats] << new_cat
   end
   
   def buy_fish(name)
     new_fish = Fish.new(name)
     @pets[:fishes] << new_fish
+  end
+  
+   def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
   end
   
   def say_species
